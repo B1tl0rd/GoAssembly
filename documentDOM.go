25a372316f7be2cc892580f3959e22a02555ae70
Drax01 <linkosoftware@gmail.com>
2018-09-05 03:30:29 -0400

    V0.1.0-Beta1 - New Page error

--- documentDOM.go
+++ documentDOM.go
@@ -7,9 +7,20 @@ import (
 
 // errorDOM Contiene una respuesta por error
 func errorDOM(errorT string) {
-	basiA := `<center> <br/><br/><br/><br/> <h1 style="color:red" >`
-	basiB := `<h1/><button onclick="goroute()">Retornar</button></center>`
-	GetElementId("app").Set("innerHTML", basiA+errorT+basiB)
+	basiA := `<center> <br/><br/><br/><br/> <h1  class="error">`
+	basiB := `<h1/><button class="btn" onclick="goroute()">Retornar</button></center>`
+
+	class := `
+	<style>
+	.error {color: #D8000C;background-color: #FFD2D2;}
+	.btn {
+		background-color: #4CAF50; border: none;color: white;
+	    padding: 15px 32px;text-align: center;text-decoration: none;
+	    display: inline-block;font-size: 16px;}
+	</style>
+	`
+	document.Set("title","Error page")
+	GetElementId("app").Set("innerHTML", basiA+errorT+basiB+class)
 	js.Global().Set("ruta", "index")
 	os.Exit(1)
 }
